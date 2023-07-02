a82280677263019e62a8727f8a5f5af26cf21289
VadimKuzminskiy <vadimkuzminskyi@gmail.com>
2023-07-02 09:11:55 +0300

    fix

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 
-const blocks = document.querySelectorAll('.block');
+// const blocks = document.querySelectorAll('.block');
 
-blocks.forEach(function(block){
-    const buttonPlus = block.querySelector('.plus');
-    const buttonMinus = block.querySelector('.minus');
-    const countElement = block.querySelector('.count');
+// blocks.forEach(function(block){
+//     const buttonPlus = block.querySelector('.plus');
+//     const buttonMinus = block.querySelector('.minus');
+//     const countElement = block.querySelector('.count');
 
-    let count = 0;
+//     let count = 0;
 
-    buttonPlus.addEventListener('click', function() {
-        count++;
-        countElement.textContent = count;
-    });
+//     buttonPlus.addEventListener('click',  function(event) {
+//         const target = event.target;
+//         const block = target.closest('.block');
+//         const countElement = block.querySelector('.count');
 
-    buttonMinus.addEventListener('click', function() {
-        count--;
-        countElement.textContent = count;
-    });
-});
+//         count++;
+//         countElement.textContent = count;
+//     });
+
+//     buttonMinus.addEventListener('click', function() {
+//         count--;
+//         countElement.textContent = count;
+//     });
+// });
+
+
+const search = document.querySelector('#container');
+search.addEventListener('click', searchClick);
+
+function searchClick(event) {
+    if
+    (event.target.className.includes('plus')) {
+        event.target.nextElementSibling.innerHTML++;
+    }
+
+    if
+    (event.target.className.includes('minus')) {
+        event.target.previousElementSibling.innerHTML--;
+    }
+}
